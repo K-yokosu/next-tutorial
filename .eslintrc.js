64e5b65a0795f51e611239d6ba5ac67bc727ee42
横田昴輝 <kokiyokotakoki@gmail.com>
2024-03-29 20:27:16 +0900

    試行錯誤2

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['next/core-web-vitals', 'prettier'],
+    ignorePatterns: ['**/.next/**', '**/node_modules/**'],
+    root: true,
+    settings: {
+      next: {
+        rootDir: ['basics/*/', 'dashboard/*/', 'seo/'],
+      },
+    },
+  };
+  

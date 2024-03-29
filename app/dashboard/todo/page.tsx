f1a6f3a1f7621a86e0ab8d08df10989bcd068215
横田昴輝 <kokiyokotakoki@gmail.com>
2024-03-29 18:44:45 +0900

    todoリスト画面の導線作成

---
+++ app/dashboard/todo/page.tsx
@@ -0,0 +1,17 @@
+// tableを作る
+// seederを流す
+// 流したデータを取得する
+// 取得したデータを表示する
+// データを作成する
+// 編集する
+// 削除する
+// ...
+
+const TodoPage = () => {
+    return(
+        <>
+            <div>todo</div>
+        </>
+    )
+}
+export default TodoPage;
